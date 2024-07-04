ca5192a9e4c29a79dd3fedf2ebad22223ffa871b
mymom0937 <mymom0937@gmail.com>
2024-07-04 10:53:11 +0100

    banner

--- netflix-clone-jan-2024/src/pages/Home/Home.js
+++ netflix-clone-jan-2024/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/Header.js";
 import Footer from "../../Components/Footer/Footer.js";
+import Banner from "../../Components/Banner/Banner.js";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Banner/>
       <Footer/>
     </>
   );
